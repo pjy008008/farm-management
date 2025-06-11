cdc2f259a652de3096244dc6dbf6546ba6f578c1
Junyou Park <pjy008008@gmail.com>
2025-06-11 19:49:05 +0900

    feat(WebConfig): 프론트엔드 통신을 위해 CORS관련 설정 추가

--- src/main/java/com/smartfarm/farm_management/config/WebConfig.java
+++ src/main/java/com/smartfarm/farm_management/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로에 대해 CORS 허용
-                .allowedOrigins("http://localhost:3000", "http://192.168.0.100:3000") // React 앱의 Origin 명시 (필요시 추가)
+                .allowedOrigins("http://localhost:5173", "http://192.168.0.100:5173", "http://127.0.0.1:5173") // React 앱의 Origin 명시 (필요시 추가)
                 // .allowedOrigins("*") // 모든 Origin 허용 (주의: 개발 단계에서만 사용 권장)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
                 .allowedHeaders("*") // 모든 헤더 허용
